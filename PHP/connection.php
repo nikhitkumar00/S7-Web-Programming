a244a565677a2678739af6e79323d90a0cec47c8
Nikhit Kumar <nikhitkumar00@gmail.com>
2024-01-07 00:27:51 +0530

    php

---
+++ PHP/connection.php
@@ -0,0 +1,14 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', '', 'invo');
+    if (connect_error()) {
+        die('Connection failed: ' . mysqli_connect_error());
+    }
+    $query = "DELETE FROM stock WHERE id = $_POST[id]";
+    $res = mysqli_query($conn, $query);
+    if($res === false) {
+        die(mysqli_error($conn));
+    }
+    while($row = mysqli_fetch_assoc($res)) {
+        echo $row['qty'];
+    }
+    ?>
